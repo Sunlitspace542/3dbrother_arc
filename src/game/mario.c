c03a950a6e03289ea6512d72de865912a4fa8477
renatone <renysci2@gmail.com>
2021-06-30 21:47:06 +0200

    modifiche sui salti del pg + lunghezza del freno

--- src/game/mario.c
+++ src/game/mario.c
@@ -822,7 +822,7 @@ static u32 set_mario_action_airborne(struct MarioState *m, u32 action, u32 actio
         case ACT_JUMP:
         case ACT_HOLD_JUMP:
             m->marioObj->header.gfx.animInfo.animID = -1;
-            set_mario_y_vel_based_on_fspeed(m, 47.0f, 0.25f);
+            set_mario_y_vel_based_on_fspeed(m, 44.0f, 0.25f);
             m->forwardVel *= 0.8f;
             break;
 

60d8f46c5c6ebaa531bd8c7b921b726e609217ea
Xdelta <47607866+Xdelta107@users.noreply.github.com>
2021-06-30 13:44:06 +0200

    rimpiazzata l'animazione del salto laterale con quella beta e fixato l'altezza del salto come nella BETAH

--- src/game/mario.c
+++ src/game/mario.c
@@ -835,7 +835,7 @@ static u32 set_mario_action_airborne(struct MarioState *m, u32 action, u32 actio
             break;
 
         case ACT_SIDE_FLIP:
-            set_mario_y_vel_based_on_fspeed(m, 45.0f, 0.0f); // 62.f
+            set_mario_y_vel_based_on_fspeed(m, 42.0f, 0.0f); // 62.f
             m->forwardVel = 8.0f;
             m->faceAngle[1] = m->intendedYaw;
             break;

3088a546ee38799c254aa0de88cb2e5124e11ad7
Xdelta <47607866+Xdelta107@users.noreply.github.com>
2021-07-05 13:44:46 +0200

    aggiunti gli alberi beta

--- actors/tree/geo.inc.c
+++ actors/tree/geo.inc.c
@@ -1,44 +1,66 @@
-// 0x16000FE8
+/********************************************************************************
+		Hms tree data.
+														[ Jan 25, 1996 ]
+*********************************************************************************/
+
+extern Gfx  RCP_main_tree[];
+extern Gfx 	RCP_tree02[];
+extern Gfx 	RCP_tree03[];
+extern Gfx 	RCP_tree04[];
+extern Gfx 	RCP_tree05[];
+
+
+/********************************************************************************/
+/*	const GeoLayout map data of MainTree.												*/
+/********************************************************************************/
 const GeoLayout bubbly_tree_geo[] = {
-   GEO_CULLING_RADIUS(800),
-   GEO_OPEN_NODE(),
-      GEO_DISPLAY_LIST(LAYER_ALPHA, tree_seg3_dl_0302FEE8),
-   GEO_CLOSE_NODE(),
-   GEO_END(),
+	GEO_CULLING_RADIUS(800),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_ALPHA, RCP_main_tree),
+	GEO_CLOSE_NODE(),
+  	GEO_END(),
 };
 
-// 0x16001000
+/********************************************************************************/
+/*	const GeoLayout map data of Tree02.												*/
+/********************************************************************************/
 const GeoLayout spiky_tree_geo[] = {
-   GEO_CULLING_RADIUS(800),
-   GEO_OPEN_NODE(),
-      GEO_DISPLAY_LIST(LAYER_ALPHA, tree_seg3_dl_03030FA0),
-   GEO_CLOSE_NODE(),
-   GEO_END(),
+	GEO_CULLING_RADIUS(800),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_ALPHA, RCP_tree02),
+	GEO_CLOSE_NODE(),
+  	GEO_END(),
 };
 
-// 0x16001018
+/********************************************************************************/
+/*	const GeoLayout map data of Tree03.												*/
+/********************************************************************************/
 const GeoLayout snow_tree_geo[] = {
-   GEO_CULLING_RADIUS(800),
-   GEO_OPEN_NODE(),
-      GEO_DISPLAY_LIST(LAYER_ALPHA, tree_seg3_dl_03032088),
-   GEO_CLOSE_NODE(),
-   GEO_END(),
+	GEO_CULLING_RADIUS(800),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_ALPHA, RCP_tree03),
+	GEO_CLOSE_NODE(),
+  	GEO_END(),
 };
 
-// 0x16001030
+/********************************************************************************/
+/*	const GeoLayout map data of Tree04.												*/
+/********************************************************************************/
 const GeoLayout spiky_tree1_geo[] = {
-   GEO_CULLING_RADIUS(800),
-   GEO_OPEN_NODE(),
-      GEO_DISPLAY_LIST(LAYER_ALPHA, tree_seg3_dl_03032170),
-   GEO_CLOSE_NODE(),
-   GEO_END(),
+	GEO_CULLING_RADIUS(800),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_ALPHA, RCP_tree04),
+	GEO_CLOSE_NODE(),
+  	GEO_END(),
 };
 
-// 0x16001048
+/********************************************************************************/
+/*	const GeoLayout map data of Tree05.												*/
+/********************************************************************************/
 const GeoLayout palm_tree_geo[] = {
-   GEO_CULLING_RADIUS(800),
-   GEO_OPEN_NODE(),
-      GEO_DISPLAY_LIST(LAYER_ALPHA, tree_seg3_dl_03033258),
-   GEO_CLOSE_NODE(),
-   GEO_END(),
+	GEO_CULLING_RADIUS(800),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_ALPHA, RCP_tree05),
+	GEO_CLOSE_NODE(),
+  	GEO_END(),
 };

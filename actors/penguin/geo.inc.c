eb9323c1828f79ae3a23237d6c4c554440f62afb
Xdelta <47607866+Xdelta107@users.noreply.github.com>
2021-07-15 11:02:59 +0200

    added beta penguin

--- actors/penguin/geo.inc.c
+++ actors/penguin/geo.inc.c
@@ -1,44 +1,60 @@
-// 0x0C000104
+/**************************************************************/
+/*	shape data ping_base (skeleton & animation) */
+/**************************************************************/
+
+extern Gfx RCP_ping_base0[];
+extern Gfx RCP_ping_base1[];
+extern Gfx RCP_ping_base2[];
+extern Gfx RCP_ping_base3[];
+extern Gfx RCP_ping_base4[];
+extern Gfx RCP_ping_base5[];
+
+extern Gfx RCP_ping_head_eye1[];
+extern Gfx RCP_ping_head_eye2[];
+extern Gfx RCP_ping_head_eye3[];
+extern Gfx RCP_ping_head_eye4[];
+extern Gfx RCP_ping_head_eye5[];
+
+
+/*-------------------------------------------------------------*/
+/*	Hms data 			                                       */
+/*-------------------------------------------------------------*/
 const GeoLayout penguin_geo[] = {
-#if defined(VERSION_EU) || defined(VERSION_SH)
-       GEO_SHADOW(SHADOW_CIRCLE_9_VERTS, 0x96, 60),
-#else
-       GEO_SHADOW(SHADOW_CIRCLE_4_VERTS, 0x96, 100),
-#endif
-       GEO_OPEN_NODE(),
-          GEO_SCALE(0x00, 16384),
-          GEO_OPEN_NODE(),
-             GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 0, 40, penguin_seg5_dl_05007540),
-             GEO_OPEN_NODE(),
-                GEO_ANIMATED_PART(LAYER_OPAQUE, -51, 84, -13, NULL),
-                GEO_OPEN_NODE(),
-                   GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 0, 0, penguin_seg5_dl_05007198),
-                GEO_CLOSE_NODE(),
-                GEO_ANIMATED_PART(LAYER_OPAQUE, 51, 84, -13, NULL),
-                GEO_OPEN_NODE(),
-                   GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 0, 0, penguin_seg5_dl_050071E8),
-                GEO_CLOSE_NODE(),
-                GEO_ANIMATED_PART(LAYER_OPAQUE, -38, -54, -13, NULL),
-                GEO_OPEN_NODE(),
-                   GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 0, 0, penguin_seg5_dl_05007238),
-                GEO_CLOSE_NODE(),
-                GEO_ANIMATED_PART(LAYER_OPAQUE, 38, -54, -13, NULL),
-                GEO_OPEN_NODE(),
-                   GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 0, 0, penguin_seg5_dl_050072C8),
-                GEO_CLOSE_NODE(),
-                GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 111, -19, NULL),
-                GEO_OPEN_NODE(),
-                   GEO_SWITCH_CASE(5, geo_switch_tuxie_mother_eyes),
-                   GEO_OPEN_NODE(),
-                      GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 0, 0, penguin_seg5_dl_05006428),
-                      GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 0, 0, penguin_seg5_dl_05006458),
-                      GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 0, 0, penguin_seg5_dl_05006488),
-                      GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 0, 0, penguin_seg5_dl_050064B8),
-                      GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 0, 0, penguin_seg5_dl_050064E8), // unused, seen in Shoshinkai 1995 footage
-                   GEO_CLOSE_NODE(),
-                GEO_CLOSE_NODE(),
-             GEO_CLOSE_NODE(),
-          GEO_CLOSE_NODE(),
-       GEO_CLOSE_NODE(),
-       GEO_END(),
+ GEO_SHADOW(SHADOW_CIRCLE_4_VERTS, 0x96, 100),
+ GEO_OPEN_NODE(),
+ GEO_SCALE(0x00, 16384),
+    GEO_OPEN_NODE(),
+      GEO_ANIMATED_PART(LAYER_OPAQUE,0,0,40,RCP_ping_base5),  /* ping_body1_2(10) */
+      GEO_OPEN_NODE(),
+         GEO_ANIMATED_PART(LAYER_OPAQUE,-51,84,-13,NULL),  /* chn21_2(9) */
+         GEO_OPEN_NODE(),
+            GEO_ANIMATED_PART(LAYER_OPAQUE,0,0,0,RCP_ping_base0),  /* ping_Rhand1_2(0) */
+         GEO_CLOSE_NODE(),
+         GEO_ANIMATED_PART(LAYER_OPAQUE,51,84,-13,NULL),  /* chn19_2(8) */
+         GEO_OPEN_NODE(),
+            GEO_ANIMATED_PART(LAYER_OPAQUE,0,0,0,RCP_ping_base1),  /* ping_Lhand1_2(1) */
+         GEO_CLOSE_NODE(),
+         GEO_ANIMATED_PART(LAYER_OPAQUE,-38,-54,-13,NULL),  /* chn24_2(7) */
+         GEO_OPEN_NODE(),
+            GEO_ANIMATED_PART(LAYER_OPAQUE,0,0,0,RCP_ping_base2),  /* ping_Rfoot1_2(2) */
+         GEO_CLOSE_NODE(),
+         GEO_ANIMATED_PART(LAYER_OPAQUE,38,-54,-13,NULL),  /* chn22_2(6) */
+         GEO_OPEN_NODE(),
+            GEO_ANIMATED_PART(LAYER_OPAQUE,0,0,0,RCP_ping_base3),  /* ping_Lfoot1_2(3) */
+         GEO_CLOSE_NODE(),
+         GEO_ANIMATED_PART(LAYER_OPAQUE,0,111,-19,NULL),  /* chn18_2(5) */
+         GEO_OPEN_NODE(),
+			GEO_SWITCH_CASE(5, geo_switch_tuxie_mother_eyes),
+			GEO_OPEN_NODE(),
+	            GEO_ANIMATED_PART(LAYER_OPAQUE,0,0,0,RCP_ping_head_eye1),  /* ping_head1_2(4) */
+	            GEO_ANIMATED_PART(LAYER_OPAQUE,0,0,0,RCP_ping_head_eye2),  /* ping_head1_2(4) */
+	            GEO_ANIMATED_PART(LAYER_OPAQUE,0,0,0,RCP_ping_head_eye3),  /* ping_head1_2(4) */
+	            GEO_ANIMATED_PART(LAYER_OPAQUE,0,0,0,RCP_ping_head_eye4),  /* ping_head1_2(4) */
+	            GEO_ANIMATED_PART(LAYER_OPAQUE,0,0,0,RCP_ping_head_eye5),  /* ping_head1_2(4) */
+			GEO_CLOSE_NODE(),
+         GEO_CLOSE_NODE(),
+      GEO_CLOSE_NODE(),
+   GEO_CLOSE_NODE(),
+ GEO_CLOSE_NODE(),
+ GEO_END(),
 };

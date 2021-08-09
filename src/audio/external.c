1493014436283783f576fc6351dffac0e2e4902f
Xdelta <47607866+Xdelta107@users.noreply.github.com>
2021-08-09 15:54:33 +0200

    increased the new boos fight volume from 70 to 85

--- src/audio/external.c
+++ src/audio/external.c
@@ -314,7 +314,7 @@ u8 sBackgroundMusicDefaultVolume[] = {
     0,   // SEQ_EVENT_CUTSCENE_LAKITU (not in JP)
     70,  // SEQ_SUMMARY
     50,  // SEQ_MEM_ERROR
-    70,  // SEQ_BOSS_FIGHT
+    85,  // SEQ_BOSS_FIGHT
 };
 
 STATIC_ASSERT(ARRAY_COUNT(sBackgroundMusicDefaultVolume) == SEQ_COUNT,
